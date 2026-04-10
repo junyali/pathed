49a12ebd62203150a3c073ce2e1ca921c666707c
mochi <git@nyamochi.moe>
2026-04-10 05:24:30 +0100

    feat: write method for on player join event

--- src/main/java/io/github/junyali/pathed/event/PathedEvents.java
+++ src/main/java/io/github/junyali/pathed/event/PathedEvents.java
@@ -1,9 +1,24 @@
 package io.github.junyali.pathed.event;
 
 import io.github.junyali.pathed.Pathed;
+import io.github.junyali.pathed.attachment.ClassAttachment;
+import io.github.junyali.pathed.attachment.PathedAttachments;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
 @EventBusSubscriber(modid = Pathed.MODID)
 public class PathedEvents {
+	@SubscribeEvent
+	public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+		if (!(event.getEntity() instanceof ServerPlayer player)) return;
 
+		ClassAttachment attachment = player.getData(PathedAttachments.CLASS_ATTACHMENT);
+
+		if (!attachment.hasChosen()) {
+			// PacketDistributor.sendToPlayer(player, classselectormethod);
+		}
+	}
 }
