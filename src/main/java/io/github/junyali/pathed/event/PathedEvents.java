a75a7ba91fc401b98dad21cf36f0e74b37a3a823
mochi <git@nyamochi.moe>
2026-04-10 05:22:30 +0100

    feat: create events class

---
+++ src/main/java/io/github/junyali/pathed/event/PathedEvents.java
@@ -0,0 +1,9 @@
+package io.github.junyali.pathed.event;
+
+import io.github.junyali.pathed.Pathed;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = Pathed.MODID)
+public class PathedEvents {
+
+}
