217fcc4d2d33c1cf43dd5a37fbdfb16b355b46b7
mochi <git@nyamochi.moe>
2026-04-11 03:37:11 +0100

    fix: update path tool builder to use ResourceLocation

--- src/main/java/io/github/junyali/pathed/data/path/PathToolBuilder.java
+++ src/main/java/io/github/junyali/pathed/data/path/PathToolBuilder.java
@@ -4,6 +4,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemLore;
@@ -15,7 +16,8 @@ import java.util.List;
 public class PathToolBuilder {
 	private PathToolBuilder() {}
 
-	public static ItemStack create(Item item, String translationKey, ChatFormatting nameColour) {
+	public static ItemStack create(Item item, ResourceLocation toolId, ChatFormatting nameColour) {
+		String key = toolId.toLanguageKey("item", "pathtool");
 		ItemStack stack = new ItemStack(item);
 
 		stack.set(DataComponents.UNBREAKABLE, new Unbreakable(true));
@@ -24,11 +26,11 @@ public class PathToolBuilder {
 		stack.set(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
 		stack.set(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY);
 		stack.set(DataComponents.CUSTOM_NAME,
-				Component.translatable(translationKey + ".name")
+				Component.translatable(key + ".name")
 						.withStyle(Style.EMPTY.withColor(nameColour).withBold(true).withItalic(false))
 		);
 		stack.set(DataComponents.LORE, new ItemLore(List.of(
-				Component.translatable(translationKey + ".description")
+				Component.translatable(key + ".description")
 						.withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY).withItalic(false))
 		)));
 		stack.set(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true);
