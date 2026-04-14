89b6467fee7b47d599720f759112ee51fd6df6e0
mochi <git@nyamochi.moe>
2026-04-14 09:08:21 +0100

    feat: add more translations and run datagen

--- src/main/java/io/github/junyali/pathed/datagen/PathedLanguageProvider.java
+++ src/main/java/io/github/junyali/pathed/datagen/PathedLanguageProvider.java
@@ -44,6 +44,8 @@ public class PathedLanguageProvider extends LanguageProvider {
 		add("pathed.gui.path_menu.title", "Your Path");
 		add("pathed.gui.path_menu.no_path", "No Path Selected");
 		add("pathed.gui.path_menu.level", "Level %s");
+		add("pathed.gui.path_menu.button.skill_tree", "Skill Tree");
+		add("pathed.gui.path_menu.button.stats", "Stats");
 
 		// KEYMAPPINGS
 
