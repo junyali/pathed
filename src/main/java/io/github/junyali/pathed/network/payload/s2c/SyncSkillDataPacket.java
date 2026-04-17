17ada6d9054a9d4e220e10840ea9a909e2d55c3f
mochi <git@nyamochi.moe>
2026-04-17 11:19:09 +0100

    feat: write necessary payload methods for s2c packet

--- src/main/java/io/github/junyali/pathed/network/payload/s2c/SyncSkillDataPacket.java
+++ src/main/java/io/github/junyali/pathed/network/payload/s2c/SyncSkillDataPacket.java
@@ -7,6 +7,7 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
@@ -105,4 +106,15 @@ public record SyncSkillDataPacket (
 					}
 				}
 			};
+
+	@Override
+	public CustomPacketPayload.@NotNull Type<? extends CustomPacketPayload> type() {
+		return TYPE;
+	}
+
+	public static void handle(SyncSkillDataPacket packet, IPayloadContext context) {
+		context.enqueueWork(() -> {
+			// send to client!
+		});
+	}
 }
