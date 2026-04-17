cd712b655cf6ce4012320f76ffe3e8359db186bc
mochi <git@nyamochi.moe>
2026-04-17 20:16:12 +0100

    feat: write class to map node type to node texture

---
+++ src/main/java/io/github/junyali/pathed/screen/NodeTextureMapper.java
@@ -0,0 +1,16 @@
+package io.github.junyali.pathed.screen;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class NodeTextureMapper {
+	public static ResourceLocation getTextureForNodeType(String nodeType, boolean completed) {
+		String state = completed ? "obtained" : "unobtained";
+
+		return switch (nodeType.toLowerCase()) {
+			case "task" -> ResourceLocation.parse("minecraft:textures/gui/sprites/advancements/task_frame_" + state + ".png");
+			case "goal" -> ResourceLocation.parse("minecraft:textures/gui/sprites/advancements/goal_frame_" + state + ".png");
+			case "challenge" -> ResourceLocation.parse("minecraft:textures/gui/sprites/advancements/challenge_frame_" + state + ".png");
+			default -> ResourceLocation.parse("minecraft:textures/gui/sprites/advancements/task_frame_" + state + ".png");
+		};
+	}
+}
