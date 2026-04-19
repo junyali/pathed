4797f9f951361d566f088a805868505d02409d00
mochi <git@nyamochi.moe>
2026-04-19 17:43:55 +0100

    feat: change player mining speed and harvest level based on their tool tier

--- src/main/java/io/github/junyali/pathed/mixin/PlayerMixin.java
+++ src/main/java/io/github/junyali/pathed/mixin/PlayerMixin.java
@@ -1,5 +1,6 @@
 package io.github.junyali.pathed.mixin;
 
+import io.github.junyali.pathed.attachment.ProgressionAttachment;
 import io.github.junyali.pathed.item.tool.IPathTool;
 import io.github.junyali.pathed.item.tool.ToolTier;
 import net.minecraft.world.entity.player.Player;
@@ -13,6 +14,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Player.class)
 public class PlayerMixin {
 	@Inject(
+			method = "getDestroySpeed",
+			at = @At("HEAD"),
+			cancellable = true
+	)
+	private void pathed_getDestroySpeed(BlockState blockState, CallbackInfoReturnable<Float> cir) {
+		Player self = (Player) (Object) this;
+		ItemStack held = self.getMainHandItem();
+		if (!(held.getItem() instanceof IPathTool)) return;
+
+		ToolTier tier = ProgressionAttachment.get(self).getUpgradeData().getCurrentToolTier();
+		cir.setReturnValue(tier.getSpeed());
+	}
+
+	@Inject(
 			method = "hasCorrectToolForDrops(Lnet/minecraft/world/level/block/state/BlockState;)Z",
 			at = @At("HEAD"),
 			cancellable = true
@@ -23,8 +38,12 @@ public class PlayerMixin {
 		if (!(held.getItem() instanceof IPathTool)) return;
 		if (blockState.getBlock().defaultDestroyTime() < 0f) return;
 
-		// ToolTier tier
-		cir.setReturnValue(true);
+		ToolTier tier = ProgressionAttachment.get(self).getUpgradeData().getCurrentToolTier();
+		if (blockState.is(tier.getIncorrectTag())) {
+			cir.setReturnValue(false);
+		} else {
+			cir.setReturnValue(true);
+		}
 		cir.cancel();
 	}
 }
