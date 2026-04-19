d06ea0e289fbf2cbfaea01fc9c4b18c36114439b
mochi <git@nyamochi.moe>
2026-04-19 17:21:07 +0100

    fix: switch mixin target

--- src/main/java/io/github/junyali/pathed/mixin/PlayerMixin.java
+++ src/main/java/io/github/junyali/pathed/mixin/PlayerMixin.java
@@ -1,6 +1,8 @@
 package io.github.junyali.pathed.mixin;
 
 import io.github.junyali.pathed.item.tool.IPathTool;
+import io.github.junyali.pathed.item.tool.ToolTier;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,17 +10,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ItemStack.class)
-public class ItemStackMixin {
+@Mixin(Player.class)
+public class PlayerMixin {
 	@Inject(
-			method = "isCorrectToolForDrops",
+			method = "hasCorrectToolForDrops(Lnet/minecraft/world/level/block/state/BlockState;)Z",
 			at = @At("HEAD"),
 			cancellable = true
 	)
-	private void pathed_isCorrectToolForDrops(BlockState blockState, CallbackInfoReturnable<Boolean> cir) {
-		ItemStack self = (ItemStack) (Object) this;
-		if (!(self.getItem() instanceof IPathTool)) return;
+	private void pathed_hasCorrectToolForDrops(BlockState blockState, CallbackInfoReturnable<Boolean> cir) {
+		Player self = (Player) (Object) this;
+		ItemStack held = self.getMainHandItem();
+		if (!(held.getItem() instanceof IPathTool)) return;
 		if (blockState.getBlock().defaultDestroyTime() < 0f) return;
+
+		// ToolTier tier
 		cir.setReturnValue(true);
 		cir.cancel();
 	}
