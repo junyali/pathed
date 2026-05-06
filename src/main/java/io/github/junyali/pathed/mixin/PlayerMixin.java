8883c304fe0135cefd90cb0f77e96533fc514f40
mochi <git@nyamochi.moe>
2026-05-06 03:07:41 +0100

    fix: remove mixin code interfering with tier system

--- src/main/java/io/github/junyali/pathed/mixin/PlayerMixin.java
+++ src/main/java/io/github/junyali/pathed/mixin/PlayerMixin.java
@@ -13,37 +13,4 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public class PlayerMixin {
-	@Inject(
-			method = "getDestroySpeed",
-			at = @At("HEAD"),
-			cancellable = true
-	)
-	private void pathed_getDestroySpeed(BlockState blockState, CallbackInfoReturnable<Float> cir) {
-		Player self = (Player) (Object) this;
-		ItemStack held = self.getMainHandItem();
-		if (!(held.getItem() instanceof IPathTool)) return;
-
-		ToolTier tier = ProgressionAttachment.get(self).getUpgradeData().getCurrentToolTier();
-		cir.setReturnValue(tier.getSpeed());
-	}
-
-	@Inject(
-			method = "hasCorrectToolForDrops(Lnet/minecraft/world/level/block/state/BlockState;)Z",
-			at = @At("HEAD"),
-			cancellable = true
-	)
-	private void pathed_hasCorrectToolForDrops(BlockState blockState, CallbackInfoReturnable<Boolean> cir) {
-		Player self = (Player) (Object) this;
-		ItemStack held = self.getMainHandItem();
-		if (!(held.getItem() instanceof IPathTool)) return;
-		if (blockState.getBlock().defaultDestroyTime() < 0f) return;
-
-		ToolTier tier = ProgressionAttachment.get(self).getUpgradeData().getCurrentToolTier();
-		if (blockState.is(tier.getIncorrectTag())) {
-			cir.setReturnValue(false);
-		} else {
-			cir.setReturnValue(true);
-		}
-		cir.cancel();
-	}
 }
