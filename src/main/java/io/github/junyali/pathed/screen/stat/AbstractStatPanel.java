be50c404e66c84d526b4e2c2c2259fc30a4dc7ee
mochi <git@nyamochi.moe>
2026-05-08 20:52:42 +0100

    feat: create class for an abstract stats panel to construct from

---
+++ src/main/java/io/github/junyali/pathed/screen/stat/AbstractStatPanel.java
@@ -0,0 +1,55 @@
+package io.github.junyali.pathed.screen.stat;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+
+public abstract class AbstractStatPanel {
+	protected int panelX;
+	protected int panelY;
+	protected int panelWidth;
+	protected int panelHeight;
+
+	protected Font font;
+
+	protected AbstractStatPanel(int x, int y, int width, int height) {
+		this.panelX = x;
+		this.panelY = y;
+		this.panelWidth = width;
+		this.panelHeight = height;
+		this.font = Minecraft.getInstance().font;
+	}
+
+	public void init(Minecraft mc) {
+
+	}
+
+	public void resize(int x, int y, int width, int height) {
+		this.panelX = x;
+		this.panelY = y;
+		this.panelWidth = width;
+		this.panelHeight = height;
+	}
+
+	public abstract void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta);
+
+	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+		return false;
+	}
+
+	public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+		return false;
+	}
+
+	public boolean charTyped(char c, int modifiers) {
+		return false;
+	}
+
+	public int getContentHeight() {
+		return panelHeight;
+	}
+
+	protected static boolean isHovered(int mouseX, int mouseY, int x, int y, int w, int h) {
+		return mouseX >= x && mouseX < x + w && mouseY >= y && mouseY < y + h;
+	}
+}
